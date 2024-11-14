0becdd3f75d1d1899970d9226e06d3d472e59ca0
karthik311099 <karthikm3110@hotmail.com>
2024-11-14 22:49:31 +0530

    store index updated

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'Medical Chatbot',
-    version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    version= '0.0.1',
+    author= 'Karthik M',
+    author_email= 'karthikm3110@hotmail.com',
     packages= find_packages(),
     install_requires = []
 
--- store_index.py
+++ store_index.py
@@ -1,16 +1,10 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
-import pinecone
+from langchain_pinecone import PineconeVectorStore
 from dotenv import load_dotenv
 import os
-
+from pinecone import Pinecone, ServerlessSpec
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
-
-# print(PINECONE_API_KEY)
-# print(PINECONE_API_ENV)
 
 extracted_data = load_pdf("data/")
 text_chunks = text_split(extracted_data)
@@ -18,11 +12,32 @@ embeddings = download_hugging_face_embeddings()
 
 
 #Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name="medical-chatbot"
 
 
-index_name="medical-bot"
+#create the pinecone index
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=384, 
+        metric="cosine", 
+        spec=ServerlessSpec(
+            cloud="aws", 
+            region="us-east-1"
+        ) 
+    ) 
+
 
 #Creating Embeddings for Each of The Text Chunks & storing
-docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
+docsearch = PineconeVectorStore.from_texts(
+        [t.page_content for t in text_chunks],
+        index_name=index_name,
+        embedding=embeddings
+    )
+
+
+
